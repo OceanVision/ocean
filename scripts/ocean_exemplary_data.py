02978cfc675804ef7f450bbba55c5886fb00f01a
kudkudak <staszek.jastrzebski@gmail.com>
2013-10-29 00:23:31 +0100

    i01t03: Decide upon graph database store format document and prepare exemplary data

---
+++ scripts/ocean_exemplary_data.py
@@ -0,0 +1,69 @@
+"""
+Exemplary data for neo4j database
+Note : wipes database !
+Connection done using RESTApi and wrapper for python py2neo
+"""
+
+from py2neo import neo4j
+from py2neo import node, rel
+
+if __name__ == "__main__":
+    # Create connection
+    graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+
+    # UNCOMMENT !!
+    print "This script will *ERASE ALL NODES AND RELATIONS IN NEO4J DATABASE*, press enter to proceed"
+    enter = raw_input()
+
+    my_batch = neo4j.ReadBatch(graph_db)
+    my_batch.append_cypher("START n=node(*) return count(n);")
+    print "Nodes in graph initially ",my_batch.submit()
+    print "Erasing nodes and relations"
+
+    my_batch = neo4j.WriteBatch(graph_db)
+    my_batch.append_cypher("start r=relationship(*) delete r;")
+    my_batch.append_cypher("start n=node(*) delete n;")
+    my_batch.submit()
+
+    my_batch = neo4j.ReadBatch(graph_db)
+    my_batch.append_cypher("START n=node(*) return count(n);")
+    result = my_batch.submit()
+    print "Nodes in graph erased. Sanity check : ",result
+
+    if result[0]!=0:
+        raise Exception("Not erased graph properly")
+        exit(1)
+
+
+    USER_LABEL = "__user__"
+    NEWS_WEBSITE_LABEL = "__news_website__"
+    NEWS_LABEL = "__news__"
+    SUBSCRIBES_TO_RELATION = "__subscribes_to___"
+    PRODUDES_RELATION = "__produces__"
+
+    # Add users
+    query = node(username="kudkudak")
+    users = [node(username="kudkudak"), node(username="konrad")] # Create abstract nodes
+    users = graph_db.create(*users) # Create nodes in graph database
+    map(lambda u: u.add_labels(USER_LABEL),users) # Add labels
+    print [x for x in  graph_db.find(USER_LABEL)] # Sanity check
+
+
+    websites = [node(url="www.antyweb.pl"), node(url="www.onet.pl"), node(url="www.wp.pl")] # Create abstract nodes
+    websites = graph_db.create(*websites) # Create nodes in graph database
+    map(lambda w: w.add_labels(NEWS_WEBSITE_LABEL),websites) # Add labels
+    print [x for x in  graph_db.find(NEWS_WEBSITE_LABEL)] # Sanity check
+
+    news = [node(url="http://konflikty.wp.pl/kat,106090,title,Nowe-smiglowce-USA-"\
+    "Wielki-projekt-zbrojeniowy-w-cieniu-budzetowych-ciec,wid,16116470,wiadomosc.html?ticaid=111908")]
+
+    news = graph_db.create(*news) # Create nodes in graph database
+    map(lambda w: w.add_labels(NEWS_LABEL),news) # Add labels
+
+    graph_db.create(rel(users[0], SUBSCRIBES_TO_RELATION, websites[2]),\
+                    rel(users[0], SUBSCRIBES_TO_RELATION, websites[1]),\
+                    rel(users[1], SUBSCRIBES_TO_RELATION, websites[1]),\
+                    rel(websites[2],PRODUDES_RELATION, news[0] )
+                    )
+
+    print "Graph populated successfully"
