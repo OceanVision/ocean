8bfc6aa3de79082e410ef2c22b82cab6e9705848
Szymon Nakoneczny <szymon.nakoneczny@gmail.com>
2013-11-22 18:10:09 +0100

    i02t03 backend method delete_channel is using cypher now

--- webservice/rss/views.py
+++ webservice/rss/views.py
@@ -103,68 +103,79 @@ def delete_channel(request):
     if request.user.is_authenticated():
         user = NeoUser.objects.filter(username__exact=request.user.username)[0]
         if user.subscribes_to.filter(link__exact=request.GET["link"]):
-            channel = user.subscribes_to.filter(link__exact=request.GET["link"])[0]
-            user.subscribes_to.remove(channel)
+            graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+            my_batch = neo4j.WriteBatch(graph_db)
+            delete_cypher = "START n=node(*) " + \
+                            "MATCH n-[rel:subscribes_t]-c " + \
+                            "WHERE HAS(n.username) AND n.username='" + request.user.username + "' AND c.link='" + request.GET["link"] + "' " + \
+                            "DELETE rel;"
+            my_batch.append_cypher(delete_cypher)
+            my_batch.submit()
 
-        return HttpResponse(content="Ok")
+            # Doesn't work because of "lazy nodes"
+            # channel = user.subscribes_to.filter(link__exact=request.GET["link"])[0]
+            # user.subscribes_to.remove(channel)
+            return HttpResponse(content="Ok")
+        else:
+            return HttpResponse(content="It's not users subscription.")
     else:
         # Redirect anonymous users to login page.
         return render(request, 'rss/message.html', {'message': 'You are not logged in'})
 
 
-        #def index(request):
-        #    #from models import *
-        #
-        #    sub_list = NeoUser.objects.get_or_create(username=request.user)[0].subscribes_to.all()
-        #    sub_str = ''
-        #
-        #    for web in sub_list:
-        #        sub_str += web.url + '; '
-        #
-        #    if request.user.is_authenticated():
-        #        # Do something for authenticated users.
-        #        return render ( request, 'rss/message.html', {
-        #            'message': 'Hello, ' + str(request.user) + '! Here are your subscriptions: ' +
-        #            sub_str
-        #            }
-        #        )
-        #    else:
-        #        # Redirect anonymous users to login page.
-        #        return render(request, 'rss/message.html', {'message': 'You are not logged in'
-        #                                                        + str(request.user.objects.filter(username__exact="admin"))})
-        #
-        #    # TODO: add as unit test
-        #
-        #    n1 = NewsWebsite.objects.filter(url="http://antyweb.pl")
-        #    if len(n1) == 0:
-        #        n1 = NewsWebsite.objects.create(url="http://antyweb.pl")
-        #        print n1._get_pk_val()
-        #        n1.save()
-        #        print "Inserted ",n1
-        #
-        #    n2 = NewsWebsite.objects.filter(url="http://spidersweb.pl")
-        #    if len(n2) == 0:
-        #        n2 = NewsWebsite.objects.create(url="http://spidersweb.pl")
-        #        n2.save()
-        #        print n2._get_pk_val()
-        #        print "Inserted ",n2
-        #
-        #    u = NeoUser.objects.filter(username="admin")
-        #    print "Found ",u
-        #
-        #    if len(u) == 0:
-        #        u = NeoUser.objects.create(username="admin")
-        #        u.save()
-        #        print "Inserted ",u
-        #
-        #        u.subscribes_to.add(n1)
-        #        u.subscribes_to.add(n2)
-        #        print "subscribed to"
-        #        print u.subscribes_to.all() #Add tests
-        #        print u._get_pk_val()
-        #        print type(u)
-        #        u.save()
-        #        n1.save()
-        #        n2.save()
-        #
-        #    #str(NeoUser.objects.filter(username__exact="admin")[0].subscribes_to.all()[]
+#def index(request):
+#    #from models import *
+#
+#    sub_list = NeoUser.objects.get_or_create(username=request.user)[0].subscribes_to.all()
+#    sub_str = ''
+#
+#    for web in sub_list:
+#        sub_str += web.url + '; '
+#
+#    if request.user.is_authenticated():
+#        # Do something for authenticated users.
+#        return render ( request, 'rss/message.html', {
+#            'message': 'Hello, ' + str(request.user) + '! Here are your subscriptions: ' +
+#            sub_str
+#            }
+#        )
+#    else:
+#        # Redirect anonymous users to login page.
+#        return render(request, 'rss/message.html', {'message': 'You are not logged in'
+#                                                        + str(request.user.objects.filter(username__exact="admin"))})
+#
+#    # TODO: add as unit test
+#
+#    n1 = NewsWebsite.objects.filter(url="http://antyweb.pl")
+#    if len(n1) == 0:
+#        n1 = NewsWebsite.objects.create(url="http://antyweb.pl")
+#        print n1._get_pk_val()
+#        n1.save()
+#        print "Inserted ",n1
+#
+#    n2 = NewsWebsite.objects.filter(url="http://spidersweb.pl")
+#    if len(n2) == 0:
+#        n2 = NewsWebsite.objects.create(url="http://spidersweb.pl")
+#        n2.save()
+#        print n2._get_pk_val()
+#        print "Inserted ",n2
+#
+#    u = NeoUser.objects.filter(username="admin")
+#    print "Found ",u
+#
+#    if len(u) == 0:
+#        u = NeoUser.objects.create(username="admin")
+#        u.save()
+#        print "Inserted ",u
+#
+#        u.subscribes_to.add(n1)
+#        u.subscribes_to.add(n2)
+#        print "subscribed to"
+#        print u.subscribes_to.all() #Add tests
+#        print u._get_pk_val()
+#        print type(u)
+#        u.save()
+#        n1.save()
+#        n2.save()
+#
+#    #str(NeoUser.objects.filter(username__exact="admin")[0].subscribes_to.all()[]
