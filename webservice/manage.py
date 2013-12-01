b2d5e7181e973d3dbf45cc7d663860b01da5f46a
kudkudak <staszek.jastrzebski@gmail.com>
2013-12-01 11:44:38 +0100

    i02t08: News discovery bot
    Bug fix

--- webservice/manage.py
+++ webservice/manage.py
@@ -9,9 +9,6 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ocean.settings")
 
-    if sys.argv[1] == "runserver":
-        import brain # work-around django recursive imports
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
