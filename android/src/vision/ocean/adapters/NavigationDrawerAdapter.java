6d459e20f75058fe991ca17c8c88b426576db43f
Szymon Nakoneczny <szymon.nakoneczny@gmail.com>
2014-03-28 18:24:19 +0100

    Iteration 5th, all tasks

---
+++ android/src/vision/ocean/adapters/NavigationDrawerAdapter.java
@@ -0,0 +1,95 @@
+package vision.ocean.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import vision.ocean.R;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class NavigationDrawerAdapter extends BaseAdapter {
+
+    private static final int TYPE_ITEM = 0;
+    private static final int TYPE_SEPARATOR = 1;
+    private static final int TYPE_MAX_COUNT = TYPE_SEPARATOR + 1;
+
+    private ArrayList mData = new ArrayList();
+    private LayoutInflater mInflater;
+
+    public TreeSet mSeparatorsSet = new TreeSet();
+
+    public NavigationDrawerAdapter(Context context) {
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void addItem(final String item) {
+        mData.add(item);
+        notifyDataSetChanged();
+    }
+
+    public void addSeparatorItem(final String item) {
+        mData.add(item);
+        // save separator position
+        mSeparatorsSet.add(mData.size() - 1);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mSeparatorsSet.contains(position) ? TYPE_SEPARATOR : TYPE_ITEM;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return TYPE_MAX_COUNT;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return String.valueOf(mData.get(position));
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        int type = getItemViewType(position);
+
+        if (convertView == null) {
+            holder = new ViewHolder();
+            switch (type) {
+                case TYPE_ITEM:
+                    convertView = mInflater.inflate(android.R.layout.simple_list_item_activated_1, null);
+                    holder.textView = (TextView) convertView.findViewById(android.R.id.text1);
+                    break;
+                case TYPE_SEPARATOR:
+                    convertView = mInflater.inflate(R.layout.item_drawer_separator, null);
+                    holder.textView = (TextView) convertView.findViewById(R.id.text_separator);
+                    break;
+            }
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.textView.setText(String.valueOf(mData.get(position)));
+        return convertView;
+    }
+
+    public static class ViewHolder {
+        public TextView textView;
+    }
+}
