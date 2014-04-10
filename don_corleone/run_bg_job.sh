1250320fbc66f7c207202ec6c84497985c64a0f9
kudkudak <staszek.jastrzebski@gmail.com>
2014-04-10 09:06:35 +0200

    Added basic error reporting (on client side)

--- don_corleone/run_bg_job.sh
+++ don_corleone/run_bg_job.sh
@@ -3,7 +3,10 @@
 function run_bg_job {
     echo `whoami`
     echo Running $1 $2
-    nohup sudo -E $2 >/dev/null 2>&1 &
+    #nohup sudo -o E $2 >/dev/null 2>&1 &
+    rm error_logs/$1.err
+    touch error_logs/$1.err
+    nohup sudo -E $2 > /dev/null 2>  error_logs/$1.err
 #    nohup sudo -E $2
 #    screen -dmSL $1 $2
 }
