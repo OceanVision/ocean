7a236fa32c335854a8de25e0a3353c9c245871f7
kudkudak <staszek.jastrzebski@gmail.com>
2014-04-10 09:36:36 +0200

    Extended logging

--- don_corleone/run_bg_job.sh
+++ don_corleone/run_bg_job.sh
@@ -5,8 +5,10 @@ function run_bg_job {
     echo Running $1 $2
     #nohup sudo -o E $2 >/dev/null 2>&1 &
     rm error_logs/$1.err
+    rm error_logs/$1.out
     touch error_logs/$1.err
-    nohup sudo -E $2 > /dev/null 2>  error_logs/$1.err
+    touch error_logs/$1.out
+    nohup sudo -E $2 > error_logs/$1.out  2>  error_logs/$1.err &
 #    nohup sudo -E $2
 #    screen -dmSL $1 $2
 }
